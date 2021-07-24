4f29bf6d06f91668446563b1decb72bef4c5bee6
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-07-24 15:05:36 +0300

    Initial Commit

---
+++ REST/RESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  RESTApp.swift
+//  REST
+//
+//  Created by Roman Fedotov on 24.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct RESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
