23131ea16ff9d1392b693289004ab61e891a75f4
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-07-26 14:20:24 +0300

    1

--- REST/ContentView.swift
+++ REST/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var posts: [Post] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                VStack {
+                    Text(post.title)
+                        .fontWeight(.black)
+                    Text(post.body)
+                }
+            } .onAppear() {
+                Api().getPost { (posts) in
+                    self.posts = posts
+                }
+            } .navigationBarTitle(Text("Posts"), displayMode: .inline)
+        }
+    }
+}
+
+struct Post: Codable, Identifiable {
+    var id: Int
+    var title: String
+    var body: String
+}
+
+class Api {
+    func getPost(completion: @escaping ([Post]) -> ()) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
+        else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let posts = try! JSONDecoder().decode([Post].self, from: data!)
+            DispatchQueue.main.async {
+                completion(posts)
+            }
+        }
+        .resume()
     }
 }
 
